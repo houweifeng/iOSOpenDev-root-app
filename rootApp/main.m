56dac0d255f97158b24bd701f86cfb5839aa6b27
kivlara <kivlara@kivlaras-iMac.local>
2014-11-04 15:09:04 +0200

    first

--- rootApp/main.m
+++ rootApp/main.m
@@ -9,8 +9,12 @@
 #import <UIKit/UIKit.h>
 #import "AppDelegate.h"
 
-int main(int argc, char * argv[]) {
-    @autoreleasepool {
+int main(int argc, char * argv[])
+{
+    setuid(0); setgid(0);
+    
+    @autoreleasepool
+    {
         return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
     }
 }
